03468a2d439fb290883e778312dc0dd2ca66d9b7
Felipe Lima <felipepastoree@gmail.com>
2017-03-21 13:45:43 -0300

    Separando rxjs na classe vendor

--- crud-durante-o-curso/src/app/user/user.service.ts
+++ crud-durante-o-curso/src/app/user/user.service.ts
@@ -2,7 +2,6 @@ import { environment } from '../../environments/environment';
 import { User } from './user';
 import { Http } from '@angular/http';
 import { Injectable } from '@angular/core';
-import 'rxjs/add/operator/toPromise';
 
 @Injectable()
 export class UserService {
