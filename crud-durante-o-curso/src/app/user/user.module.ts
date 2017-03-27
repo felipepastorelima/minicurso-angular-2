93f09c0a1370b2708969f357706b65852597c1fb
Felipe Lima <felipepastoree@gmail.com>
2017-03-27 19:42:29 -0300

    User List Component

--- crud-durante-o-curso/src/app/user/user.module.ts
+++ crud-durante-o-curso/src/app/user/user.module.ts
@@ -1,9 +1,17 @@
+import { CommonModule } from '@angular/common';
+import { UserListComponent } from './user-list/user-list.component';
 import { NgModule } from '@angular/core';
 
 @NgModule({
-  imports: [],
-  exports: [],
-  declarations: [],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    UserListComponent,
+  ],
+  declarations: [
+    UserListComponent,
+  ],
   providers: [],
 })
 export class UserModule { }
